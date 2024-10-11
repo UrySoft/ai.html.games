52a0bfa1e6088c2d2d07831193630103388fbe62
UrySoft <oriolba@gmail.com>
2024-10-11 19:40:20 +0200

    Delete js directory

--- js/words.js
+++
@@ -1,6 +0,0 @@
-const words = [
-    { word: 'CAVALL', hint: 'Animal que es munta.' },
-    { word: 'FLOR', hint: 'Part colorida d’una planta.' },
-    { word: 'LLUNA', hint: 'Satèl·lit natural de la Terra.' },
-    // Añadir más palabras según necesidad.
-];
