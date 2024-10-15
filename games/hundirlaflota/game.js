e58082f184b4d4f48048ac1dc961cc1bb5ed80ee
UrySoft <oriolba@gmail.com>
2024-10-15 22:40:51 +0200

    Add files via upload

--- games/hundirlaflota/game.js
+++ games/hundirlaflota/game.js
@@ -2,16 +2,29 @@
 let orientacion = 'horizontal'; // Inicialmente horizontal
 let barcoSeleccionado = null;
 let tamañoBarco = 0;
+const letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
-// Crear el tablero de 10x10
+// Crear el tablero de 10x10 con coordenadas
 function crearTablero(tableroID) {
     const tablero = document.getElementById(tableroID);
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
+    
+    // Añadir coordenadas horizontales (A-J)
+    for (let i = 0; i <= 10; i++) {
+        for (let j = 0; j <= 10; j++) {
             const celda = document.createElement('button');
-            celda.dataset.fila = i;
-            celda.dataset.columna = j;
-            celda.addEventListener('click', colocarBarco);
+            if (i === 0 && j === 0) {
+                celda.classList.add('coord');
+            } else if (i === 0) {
+                celda.textContent = letras[j - 1];
+                celda.classList.add('coord');
+            } else if (j === 0) {
+                celda.textContent = i;
+                celda.classList.add('coord');
+            } else {
+                celda.dataset.fila = i - 1;
+                celda.dataset.columna = j - 1;
+                celda.addEventListener('click', colocarBarco);
+            }
             tablero.appendChild(celda);
         }
     }
