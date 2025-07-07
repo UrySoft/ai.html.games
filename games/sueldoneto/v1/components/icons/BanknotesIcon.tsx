4fc10774275b8a734df1b754648b3cb411a47d42
UrySoft <oriolba@gmail.com>
2025-07-07 15:40:10 +0200

    Delete games/sueldoneto directory

--- games/sueldoneto/v1/components/icons/BanknotesIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BanknotesIcon: React.FC<{className?: string}> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414-.336.75-.75.75h-1.5m1.5 0v.375c0 .621-.504 1.125-1.125 1.125h-17.25m18-1.5v-6.75A.75.75 0 0020.25 4.5h-16.5" />
-    </svg>
-);
-
-export default BanknotesIcon;
