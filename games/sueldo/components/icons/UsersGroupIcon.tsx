1032c10638ed8bad8e8c307ab1b35d6e710645ab
UrySoft <oriolba@gmail.com>
2025-07-07 16:15:43 +0200

    Delete games/sueldo directory

--- games/sueldo/components/icons/UsersGroupIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const UsersGroupIcon: React.FC<{className?: string}> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m-7.5-2.952a4.5 4.5 0 01-1.897-1.72m3.996 5.823a5.25 5.25 0 01-3.996-5.823M3.75 10.5a3 3 0 116 0 3 3 0 01-6 0zM12 15.75a3 3 0 100-6 3 3 0 000 6z" />
-    </svg>
-);
-
-export default UsersGroupIcon;
